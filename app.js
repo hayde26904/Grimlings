9d500b1b2948265a4d6e942977effcac8809b46d
aRobert <124544863+RobertFilms@users.noreply.github.com>
2025-01-13 11:15:40 -0500

    Base dashboard page
    Created basic dashboard page to start the game
    still needs updates

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.use(localsMiddleware);
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
