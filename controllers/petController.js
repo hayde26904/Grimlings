3d329fd38c59476748466041b1750d21f6f4c54e
aRobert <124544863+RobertFilms@users.noreply.github.com>
2025-01-24 11:17:32 -0500

    Got the choose pet page to look up the users pets

--- controllers/petController.js
+++ controllers/petController.js
@@ -12,6 +12,22 @@ async function petCreationPage(req, res){
     res.render('pages/createPet', {species: petSpecies});
 }
 
+async function petChoosePage(req, res) {
+    res.render('pages/choosepet', { species: petSpecies });
+}
+
+async function choosePet(req, res) {
+    const userId = req.session.user.uid;
+
+    try {
+        const pets = ('SELECT * FROM pets WHERE user_id = ?', [userId]);
+        console.log(pets);
+        return res.render('pages/choosepet', { species: petSpecies, pets: pets });
+    } catch (error) {
+        return res.render('error', { error: new Error('Error fetching pets') });
+    }
+}
+
 async function createPet(req, res){
     const data = {
         petName: req.body.petName,
@@ -29,5 +45,7 @@ async function createPet(req, res){
 
 module.exports = {
     petCreationPage,
-    createPet
+    createPet,
+    petChoosePage,
+    choosePet
 }
--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -7,6 +7,9 @@ const petController = require('../controllers/petController');
 router.get('/createpet', isLoggedIn, petController.petCreationPage);
 router.post('/createpet', isLoggedIn, petController.createPet);
 
+router.get('/choosepet', isLoggedIn, petController.petChoosePage)
+router.post('/choosepet', isLoggedIn, petController.choosePet);
+
 router.get('/games', isLoggedIn, (req, res) => {
     res.render('pages/games');
 });
